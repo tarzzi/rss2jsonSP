c8282e109457c1fbf4726dbc32e5736911f1e9e7
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2022-05-14 00:53:31 +0300

    update get addr, logic

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const app = express()
 import rssParser from 'rss-parser';
 const parser = new rssParser();
 const port = process.env.PORT || 3000
+import bodyParser from 'body-parser';
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/*',function(req,res){
+app.get('/v1/feed',function(req,res){
 
-    var url = req.params[0];
+    let url = req.query['url'];
     
     (async () => {
         try {
             let feed = await parser.parseURL(url);
             console.log(feed.title);
             console.log("Feed was fetched");
+            res.header('http-code',304);
             res.json(feed);
         }   
         catch(err) {
